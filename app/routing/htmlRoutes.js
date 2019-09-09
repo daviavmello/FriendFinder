c685ece60e0d5f22c2b95e803b5a50f73812a15f
Davi Mello <davimello1994@gmail.com>
2019-09-09 18:00:10 -0400

    Router troubleshooting and some logic added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,12 @@
+// Path package to get the html file path
+let path = require('path');
+
 // GET route that sends the user to AJAX Page
-app.get('/survey', req,res => {
-    res.sendFile(path.join(__dirname, "survey.html"));
-})
+app.get('/survey', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+});
 
 // Route that leads to home.html
-app.get('/', req,res => {
-    res.sendFile(path.join(__dirname, "home.html"));
-})
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+});
--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
-const path = require("path");
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Router
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Listener
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+})
