e9b2ff1f119fccefb37f4e88a2b55e3dc6e137fc
Davi Mello <davimello1994@gmail.com>
2019-09-14 11:32:05 -0400

    Part of logic added. Still have more to do

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,19 +1,38 @@
 let friends = [
     {
-        name: "John",
-        photo: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg",
+        name: 'John',
+        photo: 'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg',
         scores: [
-            1,
-            4,
-            3,
-            5,
-            2,
-            4,
-            3,
-            5,
-            4,
-            2
+            '1',
+            '4',
+            '3',
+            '5',
+            '2',
+            '4',
+            '3',
+            '5',
+            '4',
+            '2'
         ]
-    }];
+    }]
+
+    // {
+    //     name: 'Lucas',
+    //     photo: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png',
+    //     scores: [
+    //         '4',
+    //         '1',
+    //         '2',
+    //         '3',
+    //         '4',
+    //         '2',
+    //         '1',
+    //         '5',
+    //         '3',
+    //         '1'
+    //     ]
+    // }];
+
+    
     
     module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,49 @@
 // Loads data source
-let friends = require('../data/friendsData');
+let friends = require("../data/friendsData");
 
 module.exports = app => {
+  // Display JSON of all possible friends
+  app.get("/api/friends", (req, res) => {
+    return res.json(friends);
+  });
 
-// Display JSON of all possible friends
-app.get('/api/friends', (req,res) => {
-    return res.json(friends)
-});
+  // Handle incoming survey results from survey.html and sends back data to frontend
+  app.post("/api/friends", (req, res) => {
+    let newFriend = req.body;
+    let currentFriendScores;
+    // let newFriendScores = newFriend.scores
+    let newArray = [];
+    for (let i = 0; i < friends.length; i++) {
+        currentFriendScores = friends[i].scores;
 
-// Handle incoming survey results from survey.html and sends back data to frontend
-app.post("/api/friends", (req,res) => {
-    let match = req.body;
+        for (i = 0; i < currentFriendScores.length; i++) {
+            console.log(newFriend.scores[i]);
+            console.log(currentFriendScores[i]);
+            
+            newArray.push(Math.abs(parseInt(newFriend.scores[i]) - parseInt(currentFriendScores[i])));
+            // console.log(newArray);
+          }
 
-    console.log(match);
-    friends.push(match);
-    res.json(friends);
+
+          
+        
+    }
+
+
+
+
+
+    console.log(newArray);
+
+
+
+
+
+
+
 
     
-}); 
+  });
+  //   friends.push(newFriend);
+  //   res.json(friends);
 };
